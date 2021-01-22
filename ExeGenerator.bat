34b7ddbb4e713f9857425d5d286f5dcb85b2d44f
Krishnamecho <krishnakumar76068@gmail.com>
2021-01-22 17:19:24 +0530

    bat file updated

--- ExeGenerator.bat
+++ ExeGenerator.bat
@@ -1,9 +1,2 @@
-set "params=%*"
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 pip install -r requirements.txt
-pyinstaller -w -F -i images/drip.ico final.py
-RD /S /Q __pycache__
-RD /S /Q build
-Del final.spec
-rename dist EXE_File
-exit
+python final.py
