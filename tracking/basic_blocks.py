b093ee6cecb6e222069a4eaf0400d855648cfd0c
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 00:16:11 +0530

    Update basic_blocks.py

--- tracking/basic_blocks.py
+++ tracking/basic_blocks.py
@@ -128,3 +128,79 @@ class Bottleneck_CI(nn.Module):
         out = center_crop(out)  # in-layer crop
 
         return out
+
+class ResNet(nn.Module):
+    """
+    ResNet with 22 layer utilized in CVPR2019 paper.
+    Usage: ResNet(Bottleneck_CI, [3, 4], [True, False], [False, True], 64, [64, 128])
+    """
+
+    def __init__(self, block, layers, last_relus, s2p_flags, firstchannels=64, channels=[64, 128], dilation=1):
+        self.inplanes = firstchannels
+        self.stage_len = len(layers)
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, firstchannels, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(firstchannels)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # stage2
+        if s2p_flags[0]:
+            self.layer1 = self._make_layer(block, channels[0], layers[0], stride2pool=True, last_relu=last_relus[0])
+        else:
+            self.layer1 = self._make_layer(block, channels[0], layers[0], last_relu=last_relus[0])
+
+        # stage3
+        if s2p_flags[1]:
+            self.layer2 = self._make_layer(block, channels[1], layers[1], stride2pool=True, last_relu=last_relus[1], dilation=dilation)
+        else:
+            self.layer2 = self._make_layer(block, channels[1], layers[1], last_relu=last_relus[1], dilation=dilation)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, last_relu, stride=1, stride2pool=False, dilation=1):
+        """
+        :param block:
+        :param planes:
+        :param blocks:
+        :param stride:
+        :param stride2pool: translate (3,2) conv to (3, 1)conv + (2, 2)pool
+        :return:
+        """
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, last_relu=True, stride=stride, downsample=downsample, dilation=dilation))
+        if stride2pool:
+            layers.append(self.maxpool)
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            if i == blocks - 1:
+                layers.append(block(self.inplanes, planes, last_relu=last_relu, dilation=dilation))
+            else:
+                layers.append(block(self.inplanes, planes, last_relu=True, dilation=dilation))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)     # stride = 2
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = center_crop7(x)
+        x = self.maxpool(x)   # stride = 4
+
+        x = self.layer1(x)
+        x = self.layer2(x)    # stride = 8
+
+        return x
