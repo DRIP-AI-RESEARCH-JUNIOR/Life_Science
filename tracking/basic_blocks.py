cd92bd382b5dca4bc9e6640b1eb0a30ff5f493b1
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-01 23:56:02 +0530

    Update basic_blocks.py

--- tracking/basic_blocks.py
+++ tracking/basic_blocks.py
@@ -2,3 +2,20 @@ import torch
 from torch import nn
 
 import numpy as np
+
+def center_crop(x):
+    """
+    center crop layer. crop [1:-2] to eliminate padding influence.
+    Crop 1 element around the tensor
+    input x can be a Variable or Tensor
+    """
+    return x[:, :, 1:-1, 1:-1].contiguous()
+
+
+def center_crop7(x):
+    """
+    Center crop layer for stage1 of resnet. (7*7)
+    input x can be a Variable or Tensor
+    """
+
+    return x[:, :, 2:-2, 2:-2].contiguous()
