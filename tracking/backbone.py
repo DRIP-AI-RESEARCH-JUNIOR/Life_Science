8dd6f5cd6808ef367d468dec73874a99c98ebae6
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 01:15:28 +0530

    Update backbone.py

--- tracking/backbone.py
+++ tracking/backbone.py
@@ -36,3 +36,42 @@ class AlexNet(nn.Module):
         x = self.feature(x)
         return x
 
+class Vgg(nn.Module):
+    """
+    Vgg backbone
+    """
+    def __init__(self):
+        super(Vgg, self).__init__()
+        self.feature_channel = 256
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 256, kernel_size=1, stride=1),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        return x
+
+
