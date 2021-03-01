78aac2187265f34a137b8d1e96c899cbdde51b2e
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 00:17:36 +0530

    Update basic_blocks.py

--- tracking/basic_blocks.py
+++ tracking/basic_blocks.py
@@ -331,3 +331,96 @@ class Inception(nn.Module):
         x = self.layer2(x)
 
         return x
+
+class BasicBlock_C(nn.Module):
+    """
+    increasing cardinality is a more effective way of
+    gaining accuracy than going deeper or wider
+    """
+
+    def __init__(self, in_planes, bottleneck_width=4, cardinality=32, expansion=2, last_relu=True):
+        super(BasicBlock_C, self).__init__()
+        inner_width = cardinality * bottleneck_width
+        self.expansion = expansion
+        self.basic = nn.Sequential(OrderedDict(
+            [
+                ('conv1_0', nn.Conv2d(in_planes, inner_width, 1, stride=1, bias=False)),
+                ('bn1', nn.BatchNorm2d(inner_width)),
+                ('act0', nn.ReLU()),
+                ('conv3_0',
+                 nn.Conv2d(inner_width, inner_width, 3, stride=1, padding=1, groups=cardinality, bias=False)),
+                ('bn2', nn.BatchNorm2d(inner_width)),
+                ('act1', nn.ReLU()),
+                ('conv1_1', nn.Conv2d(inner_width, inner_width * self.expansion, 1, stride=1, bias=False)),
+                ('bn3', nn.BatchNorm2d(inner_width * self.expansion))
+            ]
+        ))
+        self.shortcut = nn.Sequential()
+        if in_planes != inner_width * self.expansion:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, inner_width * self.expansion, 1, stride=1, bias=False)
+            )
+        self.bn0 = nn.BatchNorm2d(self.expansion * inner_width)
+        self.last_relu = last_relu
+
+    def forward(self, x):
+        out = self.basic(x)
+        out += self.shortcut(x)
+
+        if self.last_relu:
+            return center_crop(F.relu(self.bn0(out)))    # CIR
+        else:
+            return center_crop(self.bn0(out))
+
+
+class ResNeXt(nn.Module):
+    """
+    ResNeXt with 22 layer utilized in CVPR2019 paper.
+    Usage: ResNeXt([3, 4], 32, 4)
+    """
+    def __init__(self, num_blocks, cardinality, bottleneck_width, expansion=2):
+        super(ResNeXt, self).__init__()
+        self.cardinality = cardinality
+        self.bottleneck_width = bottleneck_width
+        self.in_planes = 64
+        self.expansion = expansion
+        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv0 = nn.Conv2d(3, self.in_planes, kernel_size=7, stride=2, padding=0)
+        self.bn0 = nn.BatchNorm2d(self.in_planes)
+        self.layer1 = self._make_layer(num_blocks[0],  last_relu=True)
+        self.layer2 = self._make_layer(num_blocks[1],  last_relu=False, stride2pool=True)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, num_blocks, last_relu=True, stride2pool=False):
+
+        layers = []
+        for i in range(0, num_blocks):
+            if i == num_blocks - 1:
+                layers.append(BasicBlock_C(self.in_planes, self.bottleneck_width, self.cardinality, self.expansion, last_relu=last_relu))
+            else:
+                layers.append(BasicBlock_C(self.in_planes, self.bottleneck_width, self.cardinality, self.expansion))
+            self.in_planes = self.expansion * self.bottleneck_width * self.cardinality
+
+            if i == 0 and stride2pool:
+                layers.append(self.maxpool)
+
+        self.bottleneck_width *= 2
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn0(self.conv0(x)))
+        out = self.maxpool1(out)
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+
+        return out
