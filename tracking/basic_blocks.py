7e9988dfcbf3faaa285aff89ae74e29e80c3802b
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 00:19:50 +0530

    Update basic_blocks.py

--- tracking/basic_blocks.py
+++ tracking/basic_blocks.py
@@ -524,3 +524,94 @@ class Bottleneck(nn.Module):
         out = self.relu(out)
 
         return out
+
+# for RPN++
+class ResNetPP(nn.Module):
+    def __init__(self, block, layers, used_layers):
+        self.inplanes = 64
+        super(ResNetPP, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=0,  # 3
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+
+        self.feature_size = 128 * block.expansion
+        self.used_layers = used_layers
+        layer3 = True if 3 in used_layers else False
+        layer4 = True if 4 in used_layers else False
+
+        if layer3:
+            self.layer3 = self._make_layer(block, 256, layers[2],
+                                           stride=1, dilation=2)  # 15x15, 7x7
+            self.feature_size = (256 + 128) * block.expansion
+        else:
+            self.layer3 = lambda x: x  # identity
+
+        if layer4:
+            self.layer4 = self._make_layer(block, 512, layers[3],
+                                           stride=1, dilation=4)  # 7x7, 3x3
+            self.feature_size = 512 * block.expansion
+        else:
+            self.layer4 = lambda x: x  # identity
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilation=1):
+        downsample = None
+        dd = dilation
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            if stride == 1 and dilation == 1:
+                downsample = nn.Sequential(
+                    nn.Conv2d(self.inplanes, planes * block.expansion,
+                              kernel_size=1, stride=stride, bias=False),
+                    nn.BatchNorm2d(planes * block.expansion),
+                )
+            else:
+                if dilation > 1:
+                    dd = dilation // 2
+                    padding = dd
+                else:
+                    dd = 1
+                    padding = 0
+                downsample = nn.Sequential(
+                    nn.Conv2d(self.inplanes, planes * block.expansion,
+                              kernel_size=3, stride=stride, bias=False,
+                              padding=padding, dilation=dd),
+                    nn.BatchNorm2d(planes * block.expansion),
+                )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride,
+                            downsample, dilation=dilation))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, dilation=dilation))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x_ = self.relu(x)
+        x = self.maxpool(x_)
+
+        p1 = self.layer1(x)
+        p2 = self.layer2(p1)
+        p3 = self.layer3(p2)
+        p4 = self.layer4(p3)
+
+        out = [x_, p1, p2, p3, p4]
+        out = [out[i] for i in self.used_layers]
+        if len(out) == 1:
+            return out[0]
+        else:
+            return out
