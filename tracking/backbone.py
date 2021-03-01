d1468e93fb5c13763be243d50992076055c9ac1e
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 01:14:50 +0530

    Update backbone.py

--- tracking/backbone.py
+++ tracking/backbone.py
@@ -3,3 +3,35 @@ from torch inport nn
 
 from basic_blocks import *
 
+class AlexNet(nn.Module):
+    """
+    AlexNet backbone
+    """
+    def __init__(self):
+        super(AlexNet, self).__init__()
+        self.feature_channel = 256
+        self.feature = nn.Sequential(
+            # conv1
+            nn.Conv2d(3, 96, 11, 2),
+            nn.BatchNorm2d(96, eps=1e-6, momentum=0.05),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, 2),
+            # conv2
+            nn.Conv2d(96, 256, 5, 1, groups=2),
+            nn.BatchNorm2d(256, eps=1e-6, momentum=0.05),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, 2),
+            # conv3
+            nn.Conv2d(256, 384, 3, 1),
+            nn.BatchNorm2d(384, eps=1e-6, momentum=0.05),
+            nn.ReLU(inplace=True),
+            # conv4
+            nn.Conv2d(384, 384, 3, 1, groups=2),
+            nn.BatchNorm2d(384, eps=1e-6, momentum=0.05),
+            nn.ReLU(inplace=True),
+            # conv5
+            nn.Conv2d(384, 256, 3, 1, groups=2))
+
+    def forward(self, x):
+        x = self.feature(x)
+        return x
