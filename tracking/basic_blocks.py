be824896ecd9795b772eb38186d43c2f524dc2a3
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-01 23:59:47 +0530

    Update basic_blocks.py

--- tracking/basic_blocks.py
+++ tracking/basic_blocks.py
@@ -19,3 +19,13 @@ def center_crop7(x):
     """
 
     return x[:, :, 2:-2, 2:-2].contiguous()
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+def conv3x3(in_planes, out_planes, stride=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, bias=False, dilation=dilation)
+    
