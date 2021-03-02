737fe9157f5fb7a4d446bd5f91437b2155a35b5e
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 15:17:35 +0530

    Create lr_scheduler.py

---
+++ tracking/models/lr_scheduler.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import math
+
+import numpy as np
+from torch.optim.lr_scheduler import _LRScheduler
+
+__all__ = ['LRScheduler', 'LogScheduler', 'StepScheduler', 'MultiStepScheduler', 'LinearStepScheduler', 'CosStepScheduler', 'WarmUPScheduler']
+
