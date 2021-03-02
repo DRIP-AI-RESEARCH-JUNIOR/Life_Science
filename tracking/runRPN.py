7131a5a7a8bf5b042ec74aaf8d8f21ea1948539f
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 16:06:01 +0530

    Update runRPN.py

--- tracking/runRPN.py
+++ tracking/runRPN.py
@@ -4,3 +4,31 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 from torchvision import transforms
 from utils import get_subwindow_tracking
+
+
+def generate_anchor(total_stride, scales, ratios, score_size):
+    anchor_num = len(ratios) * len(scales)
+    anchor = np.zeros((anchor_num, 4),  dtype=np.float32)
+    size = total_stride * total_stride
+    count = 0
+    for ratio in ratios:
+        # ws = int(np.sqrt(size * 1.0 / ratio))
+        ws = int(np.sqrt(size / ratio))
+        hs = int(ws * ratio)
+        for scale in scales:
+            wws = ws * scale
+            hhs = hs * scale
+            anchor[count, 0] = 0
+            anchor[count, 1] = 0
+            anchor[count, 2] = wws
+            anchor[count, 3] = hhs
+            count += 1
+
+    anchor = np.tile(anchor, score_size * score_size).reshape((-1, 4))
+    ori = - (score_size / 2) * total_stride
+    xx, yy = np.meshgrid([ori + total_stride * dx for dx in range(score_size)],
+                         [ori + total_stride * dy for dy in range(score_size)])
+    xx, yy = np.tile(xx.flatten(), (anchor_num, 1)).flatten(), \
+             np.tile(yy.flatten(), (anchor_num, 1)).flatten()
+    anchor[:, 0], anchor[:, 1] = xx.astype(np.float32), yy.astype(np.float32)
+    return anchor
