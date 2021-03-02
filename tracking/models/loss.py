53e4b1edb3bbbc1096ac03055a746f69a3434ef4
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 15:09:12 +0530

    Update loss.py

--- tracking/models/loss.py
+++ tracking/models/loss.py
@@ -3,3 +3,15 @@ import torch.nn
 import torch.nn.functional as F
 import random
 import numpy as np
+
+def rpn_cross_entropy(input, target):
+    r"""
+    :param input: (15x15x5,2)
+    :param target: (15x15x5,)
+    :return:
+    """
+    mask_ignore = target == -1
+    mask_calcu = 1 - mask_ignore
+    loss = F.cross_entropy(input=input[mask_calcu], target=target[mask_calcu], size_average=False)
+    # loss = torch.div(torch.sum(loss), 64)
+    return loss
