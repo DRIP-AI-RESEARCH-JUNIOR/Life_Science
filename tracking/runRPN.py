4ee7c2250ff351b855107ac24cf154616f2df6a7
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 16:11:13 +0530

    Update runRPN.py

--- tracking/runRPN.py
+++ tracking/runRPN.py
@@ -84,3 +84,53 @@ class TrackerConfig(object):
         for k, v in cfg.items():
             setattr(self, k, v)
         self.score_size = (self.instance_size - self.exemplar_size) / self.total_stride + 1
+
+        
+def tracker_eval(net, x_crop, target_pos, target_sz, window, scale_z, p):
+
+    delta, score = net(x_crop)
+    delta = delta.permute(1, 2, 3, 0).contiguous().view(4, -1).data.cpu().numpy()
+    score = F.softmax(score.permute(1, 2, 3, 0).contiguous().view(2, -1), dim=0).data[1, :].cpu().numpy()
+    delta[0, :] = delta[0, :] * p.anchor[:, 2] + p.anchor[:, 0]
+    delta[1, :] = delta[1, :] * p.anchor[:, 3] + p.anchor[:, 1]
+    delta[2, :] = np.exp(delta[2, :]) * p.anchor[:, 2]
+    delta[3, :] = np.exp(delta[3, :]) * p.anchor[:, 3]
+
+    def change(r):
+        return np.maximum(r, 1./r)
+
+    def sz(w, h):
+        pad = (w + h) * 0.5
+        sz2 = (w + pad) * (h + pad)
+        return np.sqrt(sz2)
+
+    def sz_wh(wh):
+        pad = (wh[0] + wh[1]) * 0.5
+        sz2 = (wh[0] + pad) * (wh[1] + pad)
+        return np.sqrt(sz2)
+
+    # size penalty
+    s_c = change(sz(delta[2, :], delta[3, :]) / (sz_wh(target_sz)))  # scale penalty
+    r_c = change((target_sz[0] / target_sz[1]) / (delta[2, :] / delta[3, :]))  # ratio penalty
+
+    penalty = np.exp(-(r_c * s_c - 1.) * p.penalty_k)
+    pscore = penalty * score
+
+    # window float
+    pscore = pscore * (1 - p.window_influence) + window * p.window_influence
+    best_pscore_id = np.argmax(pscore)
+    # print('###################### {}'.format(best_pscore_id))
+
+    target = delta[:, best_pscore_id] / scale_z
+    target_sz = target_sz / scale_z
+    lr = penalty[best_pscore_id] * score[best_pscore_id] * p.lr
+
+    res_x = target[0] + target_pos[0]
+    res_y = target[1] + target_pos[1]
+
+    res_w = target_sz[0] * (1 - lr) + target[2] * lr
+    res_h = target_sz[1] * (1 - lr) + target[3] * lr
+
+    target_pos = np.array([res_x, res_y])
+    target_sz = np.array([res_w, res_h])
+    return target_pos, target_sz, score[best_pscore_id]
