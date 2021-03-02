35cf0be87a23d25c0227ffa12e95c225042344cb
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 14:50:17 +0530

    Update builder.py

--- tracking/models/builder.py
+++ tracking/models/builder.py
@@ -13,3 +13,68 @@ from .utils import load_pretrain
 __all__ = ['SiamFC_', 'SiamFC', 'SiamVGG', 'SiamFCRes22', 'SiamFCIncep22', 'SiamFCNext22', 'SiamFCRes22W',
            'SiamRPN', 'SiamRPNVGG', 'SiamRPNRes22', 'SiamRPNIncep22', 'SiamRPNResNeXt22', 'SiamRPNPP']
 
+class SiamFC_(nn.Module):
+    def __init__(self):
+        super(SiamFC_, self).__init__()
+        self.features = None
+        # self.head = None
+
+    def head(self, z, x):
+        n, c, h, w = x.size()
+        x = x.view(1, n * c, h, w)
+        out = F.conv2d(x, z, groups=n)
+        out = out.view(n, 1, out.size(-2), out.size(-1))
+        return out
+
+    def feature_extractor(self, x):
+        return self.features(x)
+
+    def connector(self, template_feature, search_feature):
+        pred_score = self.head(template_feature, search_feature)
+        return pred_score
+
+    def branch(self, allin):
+        allout = self.feature_extractor(allin)
+        return allout
+
+    def forward(self, template, search):
+        zf = self.feature_extractor(template)
+        xf = self.feature_extractor(search)
+        score = self.connector(zf, xf)
+        return score
+
+
+class SiamFC(SiamFC_):
+
+    def __init__(self):
+        super(SiamFC, self).__init__()
+        self.features = AlexNet()
+        self._initialize_weights()
+
+    def forward(self, z, x):
+        zf = self.features(z)
+        xf = self.features(x)
+        score = self.head(zf, xf)
+        return score
+
+    def head(self, z, x):
+        # fast cross correlation
+        n, c, h, w = x.size()
+        x = x.view(1, n * c, h, w)
+        out = F.conv2d(x, z, groups=n)
+        out = out.view(n, 1, out.size(-2), out.size(-1))
+
+        # adjust the scale of responses
+        out = 0.001 * out + 0.0
+
+        return out
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight.data, mode='fan_out',
+                                     nonlinearity='relu')
+                m.bias.data.fill_(0)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
