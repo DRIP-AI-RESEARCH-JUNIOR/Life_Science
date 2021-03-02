4ae1fbd9fa4a9146ed2c17eb921beb28e13f8398
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 14:44:15 +0530

    Update builder.py

--- tracking/models/builder.py
+++ tracking/models/builder.py
@@ -1,1 +1,11 @@
-
+import math
+import torch
+from torchvision import models
+import torch.nn.functional as F
+import torch.nn.init as init
+import torch.nn as nn
+from torch.autograd import Variable
+from .heads import Corr_Up, MultiRPN, DepthwiseRPN
+from .backbones import AlexNet, Vgg, ResNet22, Incep22, ResNeXt22, ResNet22W, resnet50, resnet34, resnet18
+from neck import AdjustLayer, AdjustAllLayer
+from .utils import load_pretrain
