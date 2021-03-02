44d9b468667e08d0a1e1d8ccdfc1f1d759182239
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 14:44:59 +0530

    Update builder.py

--- tracking/models/builder.py
+++ tracking/models/builder.py
@@ -9,3 +9,4 @@ from .heads import Corr_Up, MultiRPN, DepthwiseRPN
 from .backbones import AlexNet, Vgg, ResNet22, Incep22, ResNeXt22, ResNet22W, resnet50, resnet34, resnet18
 from neck import AdjustLayer, AdjustAllLayer
 from .utils import load_pretrain
+
