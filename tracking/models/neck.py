f74f0e7455034f5c033bf745c813a9bc72194b35
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 14:29:12 +0530

    Create neck.py

---
+++ tracking/models/neck.py
@@ -0,0 +1,9 @@
+# borrowed from pysot
+# Copyright (c) SenseTime. All Rights Reserved.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import torch.nn as nn
