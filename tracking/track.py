8366a76b66cd14ae4fa936d1bbe43c6e88677ca5
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 17:25:09 +0530

    Update track.py

--- tracking/track.py
+++ tracking/track.py
@@ -17,4 +17,26 @@ cfg = {
     "output_video": True
 }
 
+class SiamRPNPP_N(nn.Module):
+    def __init__(self, tracker_name):
+        super(SiamRPNPP_N, self).__init__()
+        self.tracker_name = tracker_name
+        self.model = SiamRPNPP()
+
+    def temple(self, z):
+        zf = self.model.features(z)
+        zf = self.model.neck(zf)
+        self.zf = zf
+
+    def forward(self, x):
+        xf = self.model.features(x)
+        xf = self.model.neck(xf)
+        cls, loc = self.model.head(self.zf, xf)
+        return loc, cls
+
+
+class SiamRPNPPRes50(SiamRPNPP_N):
+    def __init__(self, tracker_name='SiamRPNPP'):
+        super(SiamRPNPPRes50, self).__init__(tracker_name)
+        self.cfg = {'lr': 0.45, 'window_influence': 0.44, 'penalty_k': 0.04, 'instance_size': 255, 'adaptive': False} # 0.355
 
