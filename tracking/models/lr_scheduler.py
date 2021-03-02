f1fcbd0359ffcc3ef5920f574355930ed8eb17f8
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 15:49:30 +0530

    Update lr_scheduler.py

--- tracking/models/lr_scheduler.py
+++ tracking/models/lr_scheduler.py
@@ -10,3 +10,22 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 __all__ = ['LRScheduler', 'LogScheduler', 'StepScheduler', 'MultiStepScheduler', 'LinearStepScheduler', 'CosStepScheduler', 'WarmUPScheduler']
 
+class LRScheduler(_LRScheduler):
+    def __init__(self, optimizer, last_epoch=-1):
+        if 'lr_spaces' not in self.__dict__:
+            raise Exception('lr_spaces must be set in "LRSchduler"')
+        super(LRScheduler, self).__init__(optimizer, last_epoch)
+
+    def get_cur_lr(self):
+        return self.lr_spaces[self.last_epoch]
+
+    def get_lr(self):
+        epoch = self.last_epoch
+        return [self.lr_spaces[epoch] * pg['initial_lr'] / self.start_lr
+                for pg in self.optimizer.param_groups]
+
+    def __repr__(self):
+        return "({}) lr spaces: \n{}".format(self.__class__.__name__,
+                                             self.lr_spaces)
+
+
