31cb2f1e9752ca3d24a7110cc5bec39a854e44ee
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-02 16:06:33 +0530

    Update runRPN.py

--- tracking/runRPN.py
+++ tracking/runRPN.py
@@ -32,3 +32,55 @@ def generate_anchor(total_stride, scales, ratios, score_size):
              np.tile(yy.flatten(), (anchor_num, 1)).flatten()
     anchor[:, 0], anchor[:, 1] = xx.astype(np.float32), yy.astype(np.float32)
     return anchor
+
+
+class TrackerConfig_SiamRPNPP(object):
+    # These are the default hyper-params for DaSiamRPN 0.3827
+    windowing = 'cosine'  # to penalize large displacements [cosine/uniform]
+    # Params from the network architecture, have to be consistent with the training
+    exemplar_size = 127  # input z size
+    instance_size = 255  # input x size (search region)
+    total_stride = 8
+    # score_size = (instance_size-exemplar_size)/total_stride+1 # for siamrpn
+    score_size = 25  # for siamrpn++
+    # print(score_size)
+    context_amount = 0.5  # context amount for the exemplar
+    ratios = [0.33, 0.5, 1, 2, 3]
+    scales = [8, ]
+    anchor_num = len(ratios) * len(scales)
+    anchor = []
+    penalty_k = 0.055
+    window_influence = 0.42
+    lr = 0.295
+    # adaptive change search region #
+    adaptive = True
+
+    def update(self, cfg):
+        for k, v in cfg.items():
+            setattr(self, k, v)
+        # self.score_size = (self.instance_size - self.exemplar_size) / self.total_stride + 1 # for siamrpn
+
+
+class TrackerConfig(object):
+    # These are the default hyper-params for DaSiamRPN 0.3827
+    windowing = 'cosine'  # to penalize large displacements [cosine/uniform]
+    # Params from the network architecture, have to be consistent with the training
+    exemplar_size = 127  # input z size
+    instance_size = 255  # input x size (search region)
+    total_stride = 8
+    score_size = (instance_size-exemplar_size)/total_stride+1
+    context_amount = 0.5  # context amount for the exemplar
+    ratios = [0.33, 0.5, 1, 2, 3]
+    scales = [8, ]
+    anchor_num = len(ratios) * len(scales)
+    anchor = []
+    penalty_k = 0.055
+    window_influence = 0.42
+    lr = 0.295
+    # adaptive change search region #
+    adaptive = True
+
+    def update(self, cfg):
+        for k, v in cfg.items():
+            setattr(self, k, v)
+        self.score_size = (self.instance_size - self.exemplar_size) / self.total_stride + 1
